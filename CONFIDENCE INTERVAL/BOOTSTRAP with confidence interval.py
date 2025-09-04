0ba130dd200d86be60b562352c67724a449fc9bb
Siddhaarth <siddhaarthsharma360@gmail.com>
2025-09-04 14:10:45 +0530

    Update BOOTSTRAP with confidence interval.py

--- CONFIDENCE INTERVAL/BOOTSTRAP with confidence interval.py
+++ CONFIDENCE INTERVAL/BOOTSTRAP with confidence interval.py
@@ -23,21 +23,22 @@ c = int(input("Enter the confidence interval here :"))
 trimed_portion = (100-c) / 2
 upper_bound = np.percentile(means2 , (100 -trimed_portion))
 lower_bound = np.percentile(means2 , 0 + trimed_portion)
-
+# 1st plot is for the original data and its mean
 plt.subplot(1 , 3 , 1)
 plt.bar(range(1 , len(X) + 1) , X , color = 'skyblue' , edgecolor = 'black')
 plt.xlabel("Sample Index")
 plt.ylabel("Value")
-
+# 2nd plot is for the bootstraped mean
 plt.subplot(1 , 3 , 2)
 plt.bar(range(1 , n+1) , means , color = 'red' , edgecolor = 'black')
 plt.xlabel("Bootstraped Index")
 plt.ylabel("Bootstraped mean")
-
+# 3rd plot for the CI interval
 plt.subplot(1 , 3 , 3)
 plt.hist(means2, bins=30, color="skyblue", edgecolor="black", alpha=0.7)
 plt.axvline(lower_bound, color="red", linestyle="--", linewidth=2, label = "Lower Bound")
 plt.axvline(upper_bound , color = "red" , linestyle = "--" , label = "Upper_bound")
 plt.show()
+
 
 
